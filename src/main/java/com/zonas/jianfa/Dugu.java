175c2976baa63deaf2a4bf45309013bdfaba4f65
tian <15140140086@163.com>
2020-03-30 00:32:31 +0800

    huashan

--- src/main/java/com/zonas/jianfa/Dugu.java
+++ src/main/java/com/zonas/jianfa/Dugu.java
@@ -3,5 +3,11 @@ package com.zonas.jianfa;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("999999jian");
+        System.out.println("999999jian");
+        System.out.println("999999jian");
+        System.out.println("999999jian");
+        System.out.println("999999jian");
+
+
     }
 }
---
+++ src/main/java/com/zonas/jianfa/Huashan.java
@@ -0,0 +1,7 @@
+package com.zonas.jianfa;
+
+public class Huashan {
+    public static void main(String[] args) {
+        System.out.println("huashan");
+    }
+}
